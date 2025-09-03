5883e02f3ba2209fdf44c7560e6fddecdef5e10c
prashantnirgun <prashant.nirgun@gmail.com>
2025-09-03 15:36:23 +0530

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # flutter-firebase
+
+## 01 Firebase Database
+
+- Create Account
+- Go to Firebase Console
+
+## 02 Prerequisites (Run from any folder)
+
+npm install -g firebase-tools
+firebase
+firebase login
+firebase projects:list
+dart pub global activate flutterfire_cli
+
+# 03 add this to ~/.zshrc (Linux User)
+
+export PATH="$PATH":"$HOME/.pub-cache/bin"
+source ~/.zshrc
+
+# 04 Run from project root
+
+flutterfire configure
+or
+flutterfire configure --project=<Project Name>
+
+# 05 pub get
+
+flutter pub add firebase_core cloud_firestore
+
+## 06 Create Schema
+
+- Create Firestore database
+- Create Collection
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_firebase/firebase_options.dart';
+import 'package:flutter_firebase/home_page.dart';
+
+void main() async {
+  //if main is async then you need to wait till app is initialised
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: HomePage(),
+    );
+  }
+}
